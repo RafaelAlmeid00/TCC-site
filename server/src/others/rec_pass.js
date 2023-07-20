18678adc40d56c056e8f9a517a1b4d8063a331f8
igão <ligor7250@gmail.com>
2023-07-20 01:30:42 -0300

    colocando as paradas no .env

--- server/src/others/rec_pass.js
+++ server/src/others/rec_pass.js
@@ -1,31 +1,37 @@
 const knex = require('../database/index');
 const nodemailer = require('nodemailer');
-const idgenerated = require('uniqid')
+const encrypt = require('bcrypt');
+const idgenerated = require('uniqid');
+const dotenv = require('dotenv');
 
 module.exports = {
 
     async EmailRec(req, res) {
         try{
+            dotenv.config();
+            console.log(process.env);
             const {cpf: cpf} = req.body;
             const [email] = await knex('user').where('user_CPF', "=", cpf);
             var recipient = email.user_email;
             console.log("this is recipient: ", recipient);
             const income = idgenerated.time();
             
-
+            const carga = await encrypt.hash(income, 10);
+            console.log('this is carga: ', carga);
+            console.log('this is user: ', process.env.USER);
             const Enterprise = nodemailer.createTransport({
                 service: 'Gmail',
                 type: 'OAuth2',
                 auth: {
-                    user: 'easypass130@gmail.com',
-                    pass: 'mjfncoxtlyucfadk',
+                    user: process.env.USER,
+                    pass: process.env.PASS,
                    },
             })
 
             const EnvTO = {
                 from: 'easypass130@gmail.com',
                 to: recipient,
-                subject: 'seu código é',
+                subject: 'seu código de recuperação é',
                 text: income,
               };
 
@@ -36,10 +42,16 @@ module.exports = {
                   console.log("Email sent successfully");
                 }
               });
-            res.status(201).send('foiskk');
+
+            res.status(201).send(carga);
         }catch{
             res.status(401).send('errokkkkkk');
         }
-    }
+    },
 
+    async compareEmail(req, res){
+        try{
+
+        }catch(erro){console.log(erro); res.send(erro)}
+    }
 }
--- src/components/cadastro/forgetPass.tsx
+++ src/components/cadastro/forgetPass.tsx
@@ -1,11 +1,16 @@
-import { Typography } from "@mui/material";
+import { Input, Typography } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
 
 
-export default function EsqueciAsenha(params:type) {
+export default function EsqueciAsenha() {
+    const [cpf, Setcpf] = useState('');
+    
     return (
         <>
-        <Typography>É isso mrm fds</Typography>
-        
+            
+            <Typography>Seu cpf: </Typography>
+            <Input onChange={i => Setcpf(i.target.value)}></Input>
         </>
     )
 }
