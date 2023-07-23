19974be9915e1c0d3da89ffa1a925eae3e8abe7b
igão <ligor7250@gmail.com>
2023-07-22 21:05:04 -0300

    22/07 sla

--- src/components/cadastro/recuperarconta/recuperation.tsx
+++ src/components/cadastro/recuperarconta/recuperation.tsx
@@ -1,22 +1,39 @@
-import { Button, Input, Typography } from "@mui/material";
+import { Button, Input, Typography, Box } from "@mui/material";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 import { useState } from "react";
+import { DeccodeItem, DecodedItem2} from "../../sistema/FrontDecoded";
 
 function EsqueciAsenha() {
     const RecToken = localStorage.getItem('item');
     const [code, Setcode] = useState('');
-    async function a() {
-        await axios.post( 'http://localhost:3344/user/login/Rec',{
+    var datadecode = {dt: DeccodeItem()};
+    console.log('this is datadecode: ', datadecode.dt.email);
+    
+    async function initialized() {
+        const response = await axios.post( 'http://localhost:3344/user/login/Rec',{
             RecToken: RecToken,
             code: code
         })
+        console.log(response);
+        localStorage.setItem('item', String(response.data.RecToken));
+        var s = DecodedItem2();
+        console.log('aaaaaaaaaaaaaaaaaaaaaa: ', s);
     }
 
     return (
         <>
-            <Typography>insira o código que enivamos para </Typography>
-            <Input onChange={i => Setcode(i.target.value)}></Input>
-            <Button onClick={a}>enviar</Button>
+            <Box sx={{
+                marginLeft: '30vw',
+                marginTop: '6vh',
+                border: 1,
+                borderRadius: 3,
+                width: '40vw',
+            }}>
+                <Typography>insira o código que enviamos para {String(datadecode.dt.email)} </Typography>
+                <Input onChange={i => Setcode(i.target.value)}></Input>
+                <Button onClick={initialized}>enviar</Button>
+            </Box>
         </>
     )
 }
--- src/components/sistema/FrontDecoded.tsx
+++ src/components/sistema/FrontDecoded.tsx
@@ -1,11 +1,22 @@
 import jwt_decode from "jwt-decode";
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Deccode() {
     const userToken = localStorage.getItem('token') || null;
     return jwt_decode(String(userToken));
 }
 
+export function DeccodeItem() {
+    const userItem = localStorage.getItem('item') || null;
+    return jwt_decode(String(userItem));
+}
+export function DecodedItem2() {
+    
+    var test = {v: jwt_decode(String(localStorage.getItem('item')))};
+    console.log('teste nesse prr: ', test.v.verified);
+    return '/cadastro/alterarSenha'
+}
 export function removeToken() {
     return localStorage.removeItem('token');
 }
