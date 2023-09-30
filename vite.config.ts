c22aaeb9891079c55eb0e26914d198e037663e1d
Rafael de Souza almeida <109623681+RafaelAlmeid00@users.noreply.github.com>
2023-09-30 17:28:39 -0300

    a
    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, 
+    host: true,
+    strictPort: true,
+    port: 5173,
   },
 })
